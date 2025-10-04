3420683a8e9f678f604a7893b4065dbdbf6c286e
marcosdgr <marcos.ciruja22@gmail.com>
2025-10-03 21:30:54 -0300

    soluciono problemas con borrado logico

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,12 +1,28 @@
 import db from "../config/db.js";
-
+// traer todos los usuarios
 export const traerUsuarios = async (req, res) => {
   try {
-    const traerTodasLosUsuarios = `SELECT * FROM USUARIOS`;
-    db.query(traerTodasLosUsuarios, (error, results) => {
+    const traerTodosLosUsuarios = `SELECT * FROM USUARIOS`;
+    db.query(traerTodosLosUsuarios, (error, results) => {
       if (error) {
         console.error("Error al traer usuarios: ", error);
         res.status(500).json({ message: "Error al traer usuarios" });
+      }
+      res.status(200).json(results);
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error del servidor" });
+  }
+};
+
+// traer usuarios activos
+export const traerUsuariosActivos = async (req, res) => {
+  try {
+    const traerSoloActivos = `SELECT * FROM USUARIOS WHERE IsActive = 1`;
+    db.query(traerSoloActivos, (error, results) => {
+      if (error) {
+        console.error("Error al traer usuarios activos: ", error);
+        res.status(500).json({ message: "Error al traer usuarios activos" });
       }
       res.status(200).json(results);
     });
@@ -21,7 +37,7 @@ export const borradoLogicoUsuario = async (req, res) => {
     const borrarUsuario = `UPDATE USUARIOS SET IsActive = 0 WHERE idUsuario = ?`;
     db.query(borrarUsuario, [idUsuario], (error, results) => {
       if (error) {
-        console.error("Error al eliminar persona: ", error);
+        console.error("Error al eliminar usuario: ", error);
         return res.status(500).json({ message: "Error al eliminar usuario" });
       }
     });
