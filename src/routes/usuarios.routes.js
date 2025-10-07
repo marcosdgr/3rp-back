5d177aef421194a7f9bc73e0d07188c4b96603b3
marcosdgr <marcos.ciruja22@gmail.com>
2025-10-07 09:53:32 -0300

    ultima actualizacion

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -4,16 +4,16 @@ import {
   traerUsuarios,
   traerUsuariosActivos,
 } from "../controllers/usuarios.controllers.js";
-import { validarRol } from "../middlewares/autenticacion.js";
+import { validarLogueado, validarRol } from "../middlewares/autenticacion.js";
 
 
 const router = Router();
 
-// Rutas públicas - sin autenticación
+// Rutas públicas - 
 router.get("/", traerUsuarios);
 router.get("/activos", traerUsuariosActivos);
 
-// Rutas de administración - sin autenticación
-router.put("/eliminar/:idUsuario", validarRol, borradoLogicoUsuario);
+// Rutas de administración - 
+router.put("/eliminar/:idUsuario", validarLogueado, validarRol, borradoLogicoUsuario);
 
 export default router;
